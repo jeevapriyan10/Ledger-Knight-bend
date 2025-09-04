7657e018f7341538ae1c2b35bda7ed760ba44ba3
jeevapriyan10 <jeeva.mail10@gmail.com>
2025-09-04 15:36:18 +0530

    path update

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false 
   //: ['http://localhost:3000']; // fallback for local dev
 
 app.use(cors({
-  origin: "https://ledger-knight-1.netlify.app/",
+  origin: "https://ledger-knight.onrender.com",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
